8003eb9f28028bea0b975725e9950082575b3eb3
João Víctor <jvhti@hotmail.com>
2018-10-06 12:00:48 -0300

    Archiving ToDo successfully;

--- js/todo.js
+++ js/todo.js
@@ -5,8 +5,14 @@ define(function () {
 	 * @module ToDo
 	 */
 
-	 let list = [];
-	 let archive = [];
+	const cooldownArchivingTime = 5; 
+	const COOLDOWN_ARCHIVING_PLACEHOLDER_START = "Will be moved to archive in ";
+	const COOLDOWN_ARCHIVING_PLACEHOLDER_END = " seconds.";
+
+	let list = [];
+	let archive = [];
+
+	let templateElem = null;
 
 	/**
 	 * Helper function to get the first parent of a element with a set tag name.
@@ -22,6 +28,21 @@ define(function () {
 	 }
 
 	/**
+	 * Create a clone of template and populate text.
+	 * @function
+	 * @name _createTemplateClone
+	 * @private
+	 * @param {String} ToDo-Text
+	 */
+	 let _createTemplateClone = function(text){
+		let added = templateElem.content.cloneNode(true);
+
+		added.querySelector("input").value = text;
+				
+		return added.querySelector("tr");
+	 }
+
+	/**
 	 * Create and append to list a new ToDo entry.
 	 * @function
 	 * @name _createToDoEntry
@@ -29,16 +50,17 @@ define(function () {
 	 * @param {String} ToDo-Text
 	 * @param {Input} New-ToDo
 	 * @param {Element} ToDo-Table
-	 * @param {Element} ToDo-Template
+	 * @param {Element} Archive-Table
 	 */
-	 let _createToDoEntry = function(text, newToDoInput, listElem, templateElem){
-		let added = templateElem.content.cloneNode(true);
-
-		added.querySelector("tr").dataset.todoid = list.length;
+	 let _createToDoEntry = function(text, newToDoInput, listElem, archiveElem){
+		let added = _createTemplateClone(text, templateElem);
 		
-		list[list.length] = added.querySelector("input").value = text;
+		added.dataset.todoid = list.length;
+		
+		list[list.length] = text;
 		
 		added.querySelector("input").addEventListener("change", _updateToDo.bind(null, listElem));
+		added.querySelector(".check-btn").addEventListener("click", _onFinisheToDo.bind(null, listElem, archiveElem));
 		
 		listElem.insertBefore(added, _getParent("TR", newToDoInput));
 	 }
@@ -50,12 +72,13 @@ define(function () {
 	 * @private
 	 * @param {Input} New-ToDo
 	 * @param {Element} ToDo-Table
+	 * @param {Element} Archive-Table
 	 * @param {Element} ToDo-Template
 	 */
-	let _addToDo = function(newToDoInput, listElem, templateElem) {
+	let _addToDo = function(newToDoInput, listElem, archiveElem, templateElem) {
 		if(newToDoInput.value.length === 0) return;
 		
-		_createToDoEntry(newToDoInput.value, newToDoInput, listElem, templateElem);
+		_createToDoEntry(newToDoInput.value, newToDoInput, listElem, archiveElem, templateElem);
 
 		newToDoInput.value = "";
 
@@ -78,6 +101,83 @@ define(function () {
 		list[target.dataset.todoid] = ev.target.value;
 		_saveToStorage();
 	}
+
+	let _onFinisheToDo = function(listElem, archiveElem, ev){
+		console.log(ev);
+		
+		let target = _getParent("TR", ev.target);
+		let input = target.querySelector("input");
+		let archiveToDo = _archiveToDo.bind(null, listElem, archiveElem);
+
+		if(target.dataset.cooldownarchiving) return _cancelMovingToArchive(target, archiveToDo);
+
+		target.dataset.cooldownarchiving = true;
+		target.dataset.cooldowntime = cooldownArchivingTime;
+
+		target.dataset.todomessage = input.value;
+		input.value = "";
+		input.placeholder = COOLDOWN_ARCHIVING_PLACEHOLDER_START+(cooldownArchivingTime)+COOLDOWN_ARCHIVING_PLACEHOLDER_END;
+
+		target.addEventListener("mouseleave", _startArchivingCountdown);
+		target.addEventListener("archivetodo", archiveToDo);
+	}
+
+	let _startArchivingCountdown = function(ev){
+		let target = _getParent("TR", ev.target);
+		let input = target.querySelector("input");
+
+		target.dataset.intervalid = setInterval(_countdown, 1000, target, input);
+		target.removeEventListener("mouseleave", _startArchivingCountdown);
+	}
+
+	let _countdown = function(target, input){
+		--target.dataset.cooldowntime;
+		
+		if(target.dataset.cooldowntime < 0){
+			target.dataset.cooldowntime = 0;
+			target.dispatchEvent(new CustomEvent("archivetodo", {detail: { todoid: target.dataset.todoid }}));
+			clearInterval(target.dataset.intervalid);
+		}
+
+		input.placeholder = COOLDOWN_ARCHIVING_PLACEHOLDER_START+(target.dataset.cooldowntime)+COOLDOWN_ARCHIVING_PLACEHOLDER_END;
+	}
+
+	let _archiveToDo = function(listElem, archiveElem, ev){
+		let message = list[ev.detail.todoid];
+		
+		_removeToDo(listElem, ev.detail.todoid);
+		
+		let added = _createTemplateClone(message);
+
+		added.dataset.archiveid = archive.length;
+		archive[archive.length] = message;
+
+		// let tmp = elem.querySelector(".check-btn");
+		// tmp.parentElement.removeChild(tmp);
+
+		archiveElem.appendChild(added);
+	}
+
+	let _cancelMovingToArchive = function(target, archiveToDo){
+		let input = target.querySelector("input");
+
+		target.removeEventListener("mouseleave", _startArchivingCountdown);
+		target.removeEventListener("archivetodo", archiveToDo);
+
+		if(target.dataset.intervalid){
+			console.log("removed Timer");
+			clearInterval(target.dataset.intervalid);
+			delete target.dataset.intervalid;
+		}
+
+		// input.value = target.dataset.todomessage;
+		input.value = list[target.dataset.todoid];
+		input.placeholder = "";
+
+		delete target.dataset.cooldownarchiving;
+		delete target.dataset.cooldowntime;
+		delete target.dataset.todomessage;
+	}
 	
 	/**
 	 * Removes the ToDo at position 'i'.
@@ -94,10 +194,11 @@ define(function () {
 		let todos = listElem.querySelectorAll("tr:not(.new-todo)");
 
 		list.splice(id, 1);
-		listElem.removeChild(todos[id]);
+		let tmp = listElem.removeChild(todos[id]);
 
 		for(let i = id; i < todos.length; ++i) --todos[i].dataset.todoid;
 		_saveToStorage();
+		return tmp;
 	}
 
 	/**
@@ -119,9 +220,10 @@ define(function () {
 	 * @private
 	 * @param {String} ToDo-Text
 	 * @param {Element} ToDo-Table
+	 * @param {Element} Archive-Table
 	 * @param {Element} ToDo-Template
 	 */
-	let _loadFromStorage = function(newToDoInput, listElem, templateElem){
+	let _loadFromStorage = function(newToDoInput, listElem, archiveElem, templateElem){
 		let newList = JSON.parse(localStorage.getItem('todos'));
 		
 		console.log("Loaded...", newList);
@@ -131,7 +233,7 @@ define(function () {
 
 		delete oldTodos;
 
-		for(let i = 0; i < newList.length; ++i) _createToDoEntry(newList[i], newToDoInput, listElem, templateElem);
+		for(let i = 0; i < newList.length; ++i) _createToDoEntry(newList[i], newToDoInput, listElem, archiveElem, templateElem);
 	}
 
 	/**
@@ -144,14 +246,15 @@ define(function () {
 	 * @param {Element} Archive-Table
 	 * @param {Element} ToDo-Template
 	 */
-	let startup = function(addButtonElem, listElem, archiveElem, templateElem){
+	let startup = function(addButtonElem, listElem, archiveElem, _templateElem){
+		templateElem = _templateElem;
 		let newToDoInput = addButtonElem.parentElement.parentElement.querySelector("input");
-		let addToDo = _addToDo.bind(null, newToDoInput, listElem, templateElem);
+		let addToDo = _addToDo.bind(null, newToDoInput, listElem, archiveElem, templateElem);
 
 		addButtonElem.addEventListener("click", addToDo);
 		newToDoInput.addEventListener("keyup", (e) => { if(e.keyCode != 13) return; addToDo();} );
 
-		_loadFromStorage(newToDoInput, listElem, templateElem);
+		_loadFromStorage(newToDoInput, listElem, archiveElem, templateElem);
 	}
 
 	return startup;
