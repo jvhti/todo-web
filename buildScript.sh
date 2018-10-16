1776a12b2760340ab61c65f4bc1ed2d0f78530d2
João Víctor <jvhti@hotmail.com>
2018-10-16 17:54:31 -0300

    Create a Build Script

---
+++ buildScript.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+echo "Building Doc"
+rm -rf ./doc/
+jsdoc js/* -d ./doc/
+
+echo "Building RequireJS"
+r.js -o name=main out=build/main-built.js baseUrl=./js optimize=none
+
+echo "Minifying RequireJS"
+babel-minify build/main-built.js -o build/main-built.min.js
+
+echo "Compiling SASS"
+sass sass/main.sass build/css/main.css --style compressed
